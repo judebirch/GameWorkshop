9871a1ce08dac60a5e1a864bfdf759e89f1a1631
Jude Birch <judebirch00@outlook.com>
2023-10-19 13:26:18 +0100

    Created previous week project

---
+++ AstroCavePrevious/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float boostPower = 40f;
+    public float torqueSpeed = 25f;
+
+    private float _currentRotation = 0f;
+    private float _currentBoost = 0f;
+
+    private Rigidbody2D _body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _body.AddTorque(-torqueSpeed * _currentRotation * Time.fixedDeltaTime);
+
+        _body.AddRelativeForce(Vector2.up * _currentBoost * boostPower);
+    }
+
+    public void OnRotate(InputAction.CallbackContext context)
+    {
+        _currentRotation = context.ReadValue<float>();
+    }
+
+    public void OnBoost(InputAction.CallbackContext context)
+    {
+        _currentBoost = context.ReadValue<float>();
+    }
+}
